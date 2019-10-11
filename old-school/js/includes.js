5bdd4eeae71cbe5f34b0ecc7efd4b0c2eb6f1ac1
Edy Segura <edysegura@gmail.com>
2019-10-11 16:18:43 -0300

    Add more info regarding the testing

--- old-school/js/includes.js
+++ old-school/js/includes.js
@@ -6,7 +6,8 @@ LazyLoader.loadFile("js/content-1.js");
 LazyLoader.loadFile("js/content-2.js");
 LazyLoader.loadFile("js/content-3.js");
 
-// just a simple test to not load the file again
+// just a simple test to check if
+// the script not load the file again
 LazyLoader.loadFile("js/content-1.js");
 LazyLoader.loadFile("js/content-2.js");
 LazyLoader.loadFile("js/content-3.js");
